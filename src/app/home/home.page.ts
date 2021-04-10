dba7f3fa37ee367e862e0daea71ad1e9bfe88115
Ariadne20 <ariliguima@gmail.com>
2021-04-10 13:33:31 -0300

    Criando página Home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,81 @@
 import { Component } from '@angular/core';
 
+type TipoPet = 'Gato'|'Cachorro';
+type Genero = 'Fêmea' | 'Macho';
+
+interface Pet{
+  Nome: string;
+  Tipo:TipoPet;
+  Genero:Genero;
+  Idade: String;
+  CorDoPelo: String;
+  CorDosOlhos:String;
+  Estado:String;
+  Cidade:String;
+  Vacinado:Boolean;
+  Foto:String;
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+
+  public pets: Pet[]=[
+    {
+      Nome:'Romeu',
+      Tipo:'Cachorro',
+      Genero: 'Macho',
+      Idade:'4 meses',
+      CorDoPelo:'Caramelo',
+      CorDosOlhos:'Castanho',
+      Estado:'SP',
+      Cidade:'Mauá',
+      Vacinado:true,
+      Foto:'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg.',
+    },
+    {
+      Nome:'Jujuba',
+      Tipo:'Gato',
+      Genero: 'Fêmea',
+      Idade:'1 ano',
+      CorDoPelo:'Cinza',
+      CorDosOlhos:'Azul',
+      Estado:'PE',
+      Cidade:'Olinda',
+      Vacinado:false,
+      Foto:'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    },
+    {
+      Nome:'Ana Clara',
+      Tipo:'Cachorro',
+      Genero:'Fêmea',
+      Idade:'2 anos',
+      CorDoPelo:'Marrom',
+      CorDosOlhos:'Castanho',
+      Estado:'SP',
+      Cidade:'Pirituba',
+      Vacinado:true,
+      Foto:'https://images.pexels.com/photos/3427343/pexels-photo-3427343.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    },
+
+    {
+      Nome:'Didi',
+      Tipo:'Gato',
+      Genero: 'Macho',
+      Idade:'4 anos',
+      CorDoPelo:'Dourado',
+      CorDosOlhos:'Mel',
+      Estado:'MG',
+      Cidade:'Ouro Preto',
+      Vacinado:false,
+      Foto:'https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    },
+  ]
 
   constructor() {}
 
