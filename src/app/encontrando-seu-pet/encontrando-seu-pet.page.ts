1a6e1de10d5fb7c5f7105e236ea9634a3c0fbbc3
Ariadne20 <ariliguima@gmail.com>
2021-06-13 14:07:30 -0300

    Favoritar Pet + Armazenamento Offline

--- src/app/encontrando-seu-pet/encontrando-seu-pet.page.ts
+++ src/app/encontrando-seu-pet/encontrando-seu-pet.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Ptor } from 'protractor';
+import {Storage} from '@ionic/storage';
 
 type TipoPet = 'Gato'|'Cachorro'| '' | 'Tudo';
 type Genero = 'Fêmea' | 'Macho';
@@ -17,6 +19,7 @@ interface Pet{
   Vacinado:Boolean;
   Foto:String;
   Descricao:String,
+  Favorito:Boolean,
 }
 
 @Component({
@@ -26,7 +29,22 @@ interface Pet{
 })
 export class EncontrandoSeuPetPage implements OnInit {
 
-  constructor() { 
+  constructor(private storage: Storage) {  
+    this.loadFromStorage();
+  }
+
+  private async loadFromStorage(){
+    const storedPets= await this.storage.get('pets-favoritados');
+    if(storedPets){
+      this.pets=storedPets;
+
+       this.listaGatos = this.pets.filter(pet=> pet.Tipo=='Gato');
+       this.listaCachorros = this.pets.filter(pet=> pet.Tipo=='Cachorro');
+      this. petsFavorito = this.pets.filter(pet=> pet.Favorito==true);
+
+
+      this.FiltroPet();
+    }
   }
 
   ngOnInit() {
@@ -46,6 +64,7 @@ export class EncontrandoSeuPetPage implements OnInit {
       Vacinado:true,
       Foto:'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg.',
       Descricao:'Romeu é um doguinho super carinhoso e bonzinho, ama caminhar e brincar.',
+      Favorito:false,
     },
     {
       id: 2,
@@ -60,6 +79,7 @@ export class EncontrandoSeuPetPage implements OnInit {
       Vacinado:false,
       Foto:'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       Descricao:'Jujuba passou por muitas dificuldades e está em busca de um lar amoroso para recomeçar.',
+      Favorito:false,
     },
     {
       id: 3,
@@ -74,7 +94,7 @@ export class EncontrandoSeuPetPage implements OnInit {
       Vacinado:true,
       Foto:'https://images.pexels.com/photos/3427343/pexels-photo-3427343.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       Descricao:'Romeu é um doguinho super carinhoso e bonzinho, ama caminhar e brincar.',
-
+      Favorito:false,
     },
     {
       id: 42,
@@ -89,13 +109,15 @@ export class EncontrandoSeuPetPage implements OnInit {
       Vacinado:false,
       Foto:'https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       Descricao:'Romeu é um doguinho super carinhoso e bonzinho, ama caminhar e brincar.',
+      Favorito:false,
     }
   ]
 
-  public inputType:TipoPet;
+  public inputType:TipoPet = '';
   public petFiltrados: Pet[] = this.pets;
   public listaGatos: Pet[] = this.pets.filter(pet=> pet.Tipo=='Gato');
   public listaCachorros: Pet[] = this.pets.filter(pet=> pet.Tipo=='Cachorro');
+  public petsFavorito: Pet[] = this.pets.filter(pet=> pet.Favorito==true);
 
   public FiltroPet(){
     if(this.inputType==''||this.inputType=='Tudo'){
@@ -107,4 +129,15 @@ export class EncontrandoSeuPetPage implements OnInit {
       this.petFiltrados=this.listaGatos;
     }
   }
+
+  public UpdateStatus(id){
+    console.log(id);
+    const index = this.pets.map(i => i.id).indexOf(id);
+    if(this.pets[index].Favorito == true){
+      this.pets[index].Favorito = false;
+    }else{
+      this.pets[index].Favorito = true;
+      }
+      this.storage.set('pets-favoritados', this.pets);
+    }
 }
