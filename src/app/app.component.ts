789550ef12e1982687b23c22ed4ebfc4bf46e8e7
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-06-15 21:16:08 -0300

    corrigindo conflitos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  constructor(public storage:Storage) {
+export class AppComponent implements OnInit {
+  constructor(public storage: Storage) {
   }
 
-  async ngOnInit(){
+  async ngOnInit() {
     await this.storage.create();
   }
 }
